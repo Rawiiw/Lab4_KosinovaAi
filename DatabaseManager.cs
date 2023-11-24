f0fea6c849d3b209e22601ec88d29c8db0f30406
Alexandra <79237095+Rawiiw@users.noreply.github.com>
2023-11-25 04:30:48 +0700

    Библиотека
    библиотека классов для работы с базой данных.
     БД содержит одну таблицу, поля таблицы: ID (уникальное целое
    число), name (строка до 256 символов), message (строка до 8096
    символов).
     Реализовано пять операций (методов) для работы с БД: GetByID
    (получение записи по заданному ID); GetByName (получение
    записей по заданному name); Add (добавление записи с указанием
    {ID, name, message}); Update (изменение у записи message по
    заданному ID); Delete (удаление записи по заданному ID);

---
+++ DatabaseManager.cs
@@ -0,0 +1,70 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace lab4_KosinovaAiLib
+{
+
+    public class DatabaseManager
+    {
+        private readonly string connectionString;
+
+        public DatabaseManager(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public Message GetByID(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                return connection.QueryFirstOrDefault<Message>("SELECT * FROM Messages WHERE ID = @ID", new { ID = id });
+            }
+        }
+
+        public List<Message> GetByName(string name)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                return connection.Query<Message>("SELECT * FROM Messages WHERE Name = @Name", new { Name = name }).ToList();
+            }
+        }
+
+        public void Add(Message message)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                connection.Execute("INSERT INTO Messages (Name, Message) VALUES (@Name, @Message)", message);
+            }
+        }
+
+        public void Update(int id, string newMessage)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                connection.Execute("UPDATE Messages SET Message = @Message WHERE ID = @ID", new { ID = id, Message = newMessage });
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                connection.Execute("DELETE FROM Messages WHERE ID = @ID", new { ID = id });
+            }
+        }
+    }
+
+    public class Message
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string MessageText { get; set; }
+    }
+}
